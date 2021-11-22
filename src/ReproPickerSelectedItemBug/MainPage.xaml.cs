e9bdb01de72dd4c2d310a036ad7fb84d90791973
Pieter Nijs <pieternijs@live.be>
2021-11-22 13:42:57 +0100

    repro

---
+++ src/ReproPickerSelectedItemBug/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReproPickerSelectedItemBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); 
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ThePicker.ItemsSource = Enumerable.Range(10, 20).ToList();
+        }
+
+        void ChangeItem_Clicked(object sender, EventArgs e)
+            => ThePicker.SelectedItem = ThePicker.ItemsSource[new Random().Next(ThePicker.ItemsSource.Count)];
+        
+        void ChangeIndex_Clicked(object sender, EventArgs e)
+            => ThePicker.SelectedIndex = new Random().Next(ThePicker.ItemsSource.Count);
+
+        private void Check_Clicked(object sender, EventArgs e)
+        {
+            Result.Text = $"SelectedItem: {ThePicker.SelectedItem}, SelectedIndex:{ThePicker.SelectedIndex}";
+        }
+    }
+}
